757421bcc6f30ac1198b37a18ae38370b9e34181
domgregori <dom.gregori@gmail.com>
2024-10-14 17:37:21 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 set -xe
 
 sudo apt install -y ipcalc
-curl -sSL https://raw.githubusercontent.com/domgregori/lxc-ipnotes/main/lxc-iptag -o /usr/local/bin/lxc-ipnotes
-curl -sSL https://raw.githubusercontent.com/domgregori/lxc-ipnotes/main/lxc-iptag.service -o /lib/systemd/system/lxc-ipnotes.service
+curl -sSL https://raw.githubusercontent.com/domgregori/lxc-ipnotes/main/lxc-ipnotes -o /usr/local/bin/lxc-ipnotes
+curl -sSL https://raw.githubusercontent.com/domgregori/lxc-ipnotes/main/lxc-ipnotes.service -o /lib/systemd/system/lxc-ipnotes.service
 chmod +x /usr/local/bin/lxc-ipnotes
 
 sudo systemctl daemon-reload
