370879a481a72090f64665c1f1756a839192964e
domgregori <dom.gregori@gmail.com>
2024-10-14 17:36:51 +0000

    Change to notes

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@
 set -xe
 
 sudo apt install -y ipcalc
-curl -sSL https://raw.githubusercontent.com/gitsang/lxc-iptag/main/lxc-iptag -o /usr/local/bin/lxc-iptag
-curl -sSL https://raw.githubusercontent.com/gitsang/lxc-iptag/main/lxc-iptag.service -o /lib/systemd/system/lxc-iptag.service
-chmod +x /usr/local/bin/lxc-iptag
+curl -sSL https://raw.githubusercontent.com/domgregori/lxc-ipnotes/main/lxc-iptag -o /usr/local/bin/lxc-ipnotes
+curl -sSL https://raw.githubusercontent.com/domgregori/lxc-ipnotes/main/lxc-iptag.service -o /lib/systemd/system/lxc-ipnotes.service
+chmod +x /usr/local/bin/lxc-ipnotes
 
 sudo systemctl daemon-reload
-sudo systemctl enable lxc-iptag.service
-sudo systemctl start lxc-iptag.service
+sudo systemctl enable lxc-ipnotes.service
+sudo systemctl start lxc-ipnotes.service
